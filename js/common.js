fcbe7ea4cde5d138af7b0ec92942cd7bc5cca9d1
Yuriy <prezlyata.yuriy@gmail.com>
2018-01-09 18:50:41 +0200

    first commit

---
+++ js/common.js
@@ -0,0 +1,10 @@
+var contacts = [];
+
+function add_contacts() {
+    var firstname = document.getElementById('name').value;
+    var lastname = document.getElementById('lastname').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var date = document.getElementById('date').value;
+    console.log(phone);
+};
