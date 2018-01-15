7347026a6b4773a4d3d731f2dbabd8c960121237
Yuriy <prezlyata.yuriy@gmail.com>
2018-01-15 12:51:13 +0200

    add validation

--- js/common.js
+++ js/common.js
@@ -17,50 +17,73 @@ function add_contacts() {
     var email = document.getElementById('email').value;
     var phone = document.getElementById('phone').value;
     var date = document.getElementById('date').value;
+    var re = /^[a-z][a-zA-Z0-9_.]*(\.[a-zA-Z][a-zA-Z0-9_.]*)?@[a-z][a-zA-Z-0-9]*\.[a-z]+(\.[a-z]+)?$/;
 
-    var new_contact = new Contact(firstname, lastname, email, phone, date);
-    contacts.push(new_contact);
-    this.reset_form();
+    if(firstname !== '' && lastname !== '' && email !== '' && re.test(email) !== false && phone !== '' && date !== '') {
 
-    var list = document.getElementById('list');
-    var contact = document.createElement('div');
-    contact.className = 'contact';
-    var firstname = document.createElement('h3');
-    var lastname = document.createElement('h3');
-    var email = document.createElement('p');
-    var phone = document.createElement('p');
-    var date = document.createElement('p');
-    var remove = document.createElement('button');
-    remove.innerHTML = 'Remove';
-    remove.className = 'btn';
+       var new_contact = new Contact(firstname, lastname, email, phone, date);
+       contacts.push(new_contact);
 
-    /*  Remove item from list function   */
-    remove.onclick = function() {
-        var listItem = this.parentNode;
-        list = listItem.parentNode;
-        list.removeChild(listItem);
-    };
+       this.reset_form();
 
-    contacts.forEach((item, index) => {
-        firstname.appendChild(document.createTextNode(item.firstname));
-        lastname.appendChild(document.createTextNode(item.lastname));
-        email.appendChild(document.createTextNode(item.email));
-        phone.appendChild(document.createTextNode(item.phone));
-        date.appendChild(document.createTextNode(item.date));
-    });
+       var list = document.getElementById('list');
+       var contact = document.createElement('div');
+       contact.className = 'contact';
+       var firstname = document.createElement('h3');
+       var lastname = document.createElement('h3');
+       var email = document.createElement('p');
+       var phone = document.createElement('p');
+       var date = document.createElement('p');
+       var remove = document.createElement('button');
+       remove.innerHTML = 'Remove';
+       remove.className = 'btn';
 
-    contact.appendChild(firstname);
-    contact.appendChild(lastname);
-    contact.appendChild(email);
-    contact.appendChild(phone);
-    contact.appendChild(date);
-    contact.appendChild(remove);
-    list.appendChild(contact);
-    console.log(contacts);
+       /*  Remove item from list function   */
+       remove.onclick = function() {
+           var listItem = this.parentNode;
+           list = listItem.parentNode;
+           list.removeChild(listItem);
+       };
+
+       contacts.forEach(item => {
+           firstname.appendChild(document.createTextNode(item.firstname));
+           lastname.appendChild(document.createTextNode(item.lastname));
+           email.appendChild(document.createTextNode(item.email));
+           phone.appendChild(document.createTextNode(item.phone));
+           date.appendChild(document.createTextNode(item.date));
+       });
+
+       contact.appendChild(firstname);
+       contact.appendChild(lastname);
+       contact.appendChild(email);
+       contact.appendChild(phone);
+       contact.appendChild(date);
+       contact.appendChild(remove);
+       list.appendChild(contact);
+   } else {
+        document.getElementById('firstname').classList.add('warning');
+        document.getElementById('lastname').classList.add('warning');
+        document.getElementById('email').classList.add('warning');
+        document.getElementById('phone').classList.add('warning');
+        document.getElementById('date').classList.add('warning');
+    }
+
+    /*  Clean our array after submit to list  */
+    contacts.splice(-1);
 };
 
 
 /*   Function reset our form   */
 function reset_form() {
     document.getElementById('form').reset();
+    this.default_input();
+}
+
+/*   Function reset our inputs   */
+function default_input() {
+    document.getElementById('firstname').classList.remove('warning');
+    document.getElementById('lastname').classList.remove('warning');
+    document.getElementById('email').classList.remove('warning');
+    document.getElementById('phone').classList.remove('warning');
+    document.getElementById('date').classList.remove('warning');
 }
